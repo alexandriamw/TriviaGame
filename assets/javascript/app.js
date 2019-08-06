34c509e1054e65381b59fc364eb7c8fc0b32001d
alexandriamw <alexandriawagner84@gmail.com>
2019-08-06 17:22:15 -0500

    started index.html with basic layout, made an array with trivia questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+ const questions = [{
+         question: "Dendrophobia is the fear of what?",
+         answers: {
+             a: "beards",
+             b: "trees",
+             c: "cooking",
+             d: "germs"
+         },
+         correctAnswer: "b"
+     },
+     {
+         question: "Porphyrophobia is the fear of what?",
+         answers: {
+             a: "birds",
+             b: "hospitals",
+             c: "the color purple",
+             d: "reptiles"
+         },
+         correctAnswer: "c"
+     },
+     {
+         question: "Gamophobia is the fear of what?",
+         answers: {
+             a: "marriage",
+             b: "gambling",
+             c: "clocks",
+             d: "books"
+         },
+         correctAnswer: "a"
+     },
+     {
+         question: "Barophobia is the fear of what?",
+         answers: {
+             a: "dentists",
+             b: "blood",
+             c: "the color black",
+             d: "gravity"
+         },
+         correctAnswer: "d"
+     },
+     {
+         question: "Noctiphobia is the fear of what?",
+         answers: {
+             a: "snow",
+             b: "rain",
+             c: "the night",
+             d: "sleep"
+         },
+         correctAnswer: "c"
+     },
+     {
+         question: "Ombrophobia is the fear of what?",
+         answers: {
+             a: "feet",
+             b: "rain",
+             c: "school",
+             d: "color"
+         },
+         correctAnswer: "b"
+     },
+     {
+         question: "Entomophobia is the fear of what?",
+         answers: {
+             a: "insects",
+             b: "colors",
+             c: "snow",
+             d: "mirrors"
+         },
+         correctAnswer: "a"
+     },
+     {
+         question: "Coulrophobia is the fear of what?",
+         answers: {
+             a: "colors",
+             b: "clowns",
+             c: "gravity",
+             d: "numbers"
+         },
+         correctAnswer: "b"
+     }
+ ]
