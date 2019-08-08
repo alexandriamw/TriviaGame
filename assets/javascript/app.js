f348783170225eda41358d4f5b54695965d85931
alexandriamw <alexandriawagner84@gmail.com>
2019-08-07 22:41:28 -0500

    took questions out of array and put in html form with radio buttons, made div for results, completed JavaScript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,69 @@
- const questions = [{
-         question: "Dendrophobia is the fear of what?",
-         answers: {
-             a: "beards",
-             b: "trees",
-             c: "cooking",
-             d: "germs"
-         },
-         correctAnswer: "b"
-     },
-     {
-         question: "Porphyrophobia is the fear of what?",
-         answers: {
-             a: "birds",
-             b: "hospitals",
-             c: "the color purple",
-             d: "reptiles"
-         },
-         correctAnswer: "c"
-     },
-     {
-         question: "Gamophobia is the fear of what?",
-         answers: {
-             a: "marriage",
-             b: "gambling",
-             c: "clocks",
-             d: "books"
-         },
-         correctAnswer: "a"
-     },
-     {
-         question: "Barophobia is the fear of what?",
-         answers: {
-             a: "dentists",
-             b: "blood",
-             c: "the color black",
-             d: "gravity"
-         },
-         correctAnswer: "d"
-     },
-     {
-         question: "Noctiphobia is the fear of what?",
-         answers: {
-             a: "snow",
-             b: "rain",
-             c: "the night",
-             d: "sleep"
-         },
-         correctAnswer: "c"
-     },
-     {
-         question: "Ombrophobia is the fear of what?",
-         answers: {
-             a: "feet",
-             b: "rain",
-             c: "school",
-             d: "color"
-         },
-         correctAnswer: "b"
-     },
-     {
-         question: "Entomophobia is the fear of what?",
-         answers: {
-             a: "insects",
-             b: "colors",
-             c: "snow",
-             d: "mirrors"
-         },
-         correctAnswer: "a"
-     },
-     {
-         question: "Coulrophobia is the fear of what?",
-         answers: {
-             a: "colors",
-             b: "clowns",
-             c: "gravity",
-             d: "numbers"
-         },
-         correctAnswer: "b"
-     }
- ]
+// curated from https://www.dyn-web.com/tutorials/forms/radio/get-selected.php
+function getRadioVal(form, name) {
+    var val;
+    // get list of radio buttons with specified name
+    var radios = form.elements[name];
+
+    // loop through list of radio buttons
+    for (var i = 0, len = radios.length; i < len; i++) {
+        if (radios[i].checked) { // radio checked?
+            val = radios[i].value; // if so, hold its value in val
+            break; // and break out of for loop
+        }
+    }
+    return val; // return value of checked radio or undefined if none checked
+}
+
+
+
+//computer waits for user to press start, then hides intro and shows questions
+document.getElementById("startTrivia").addEventListener("click", function () {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("questions").style.display = "block";
+
+    const timerEl = document.getElementById("timer");
+    let timeLeft = 30;
+    const timerInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+        if (timeLeft === 0) {
+            tally();
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+});
+
+document.getElementById("questions").addEventListener("submit", function (event) {
+    event.preventDefault();
+    tally();
+});
+
+function tally() {
+    let answers = ["b", "c", "a", "d", "c", "b", "a", "b"];
+    let correct = 0;
+    let wrong = 0;
+    let unanswered = 0;
+    const correctEl = document.getElementById("correct");
+    const wrongEl = document.getElementById("wrong");
+    const unansweredEl = document.getElementById("unanswered");
+
+    for (let i = 0; i < answers.length; i++) {
+        const answer = getRadioVal(document.getElementById("questions"), "q" + (i + 1));
+
+        if (answer === answers[i]) {
+            correct++;
+        } else {
+            if (answer === undefined) {
+                unanswered++;
+            } else {
+                wrong++;
+            }
+        }
+    }
+
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("results").style.display = "block";
+    correctEl.textContent = correct;
+    wrongEl.textContent = wrong;
+    unansweredEl.textContent = unanswered;
+}
